7cb4578874e6001cebd029cecf6aec11e1c3e8a5
frizzy <dhanikata@gmail.com>
2020-03-25 22:49:14 +0700

    Update covid.py

--- userbot/modules/covid.py
+++ userbot/modules/covid.py
@@ -1,3 +1,18 @@
+# Copyright (C) 2019 The Raphielscape Company LLC.
+#
+# Licensed under the Raphielscape Public License, Version 1.c (the "License");
+# you may not use this file except in compliance with the License.
+#
+""" Userbot module containing commands related to android"""
+
+import re
+from requests import get
+from bs4 import BeautifulSoup
+
+from userbot import CMD_HELP
+from userbot.events import register
+
+
 @register(outgoing=True, pattern="^.covid.global$")
 async def covid_global(request):
     """ covid-19 global """
@@ -10,3 +25,7 @@ async def covid_global(request):
         data = get(release_url).json()
         releases += f'{OBJECTID}: ["Country_region"]["Last_Update"]["Lat"]["Long"]["Confirmed"]["Deaths"]["Recovered"]["Active"]\n'
     await request.edit(releases)
+
+    CMD_HELP.update({
+    "covid": ".covid.global\nUsage : Give information about Corona Virus in your country"
+})
