6a03a11485981a2e9ec327de31454f7fa1a74273
frizzy <dhanikata@gmail.com>
2020-03-25 19:06:48 +0700

    nyoba ¯\_(ツ)_/¯

---
+++ userbot/modules/covid_id.py
@@ -0,0 +1,18 @@
+import json
+import requests
+
+from userbot import CMD_HELP
+from userbot.events import register
+
+@register(outgoing=True, pattern="^.covidid$")
+async def get_covidid(e):
+    await e.edit(
+    
+    url = https://api.kawalcorona.com/indonesia/provinsi.json
+    request = requests.get(url)
+    result = json.loads(request.text)
+    
+CMD_HELP.update({
+        "covidid": ".covidid\
+        \nUsage: Get covid-19 data in Indonesia\n"
+    })
