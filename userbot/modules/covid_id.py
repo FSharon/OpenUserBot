5847b17d512b336c60447b30299602a3f1ba74cf
frizzy <dhanikata@gmail.com>
2020-03-25 20:43:07 +0700

    Again ¯\_(ツ)_/¯

--- userbot/modules/covid_id.py
+++ userbot/modules/covid_id.py
@@ -8,9 +8,25 @@ from userbot.events import register
 async def get_covidid(e):
     await e.edit(
     
-    url = f'https://api.kawalcorona.com/indonesia/provinsi.json'
+    url = https://api.kawalcorona.com/indonesia/provinsi.json
     request = requests.get(url)
     result = json.loads(request.text)
+    
+    if request.status_code != 200:
+        await e.edit(f"{result['status_description']}")
+        return
+        
+        provinsi = result["provinsi"]
+        positif = result["kasus_posi"]
+        sembuh = result["kasus_semb"]
+        meninggal = result["kasus_meni"]
+        
+        textkirim = (f"**Provinsi :** `{provinsi}`\n" +
+                                f"**Positif :**  `{kasus_posi}`\n\n" +
+                                f"**Sembuh :** `{kasus_semb}`\n" +
+                                f"**Meninggal :** `{kasus_meni}`\n")
+                                
+        await e.edit(textkirim)
     
 CMD_HELP.update({
         "covidid": ".covidid\
