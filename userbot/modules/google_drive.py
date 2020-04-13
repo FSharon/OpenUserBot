94a65dddb3bf835cf58d9c316f069c4364ff8d97
frizzy <dhanikata@gmail.com>
2020-04-13 19:09:23 +0700

    Update google_drive.py

--- userbot/modules/google_drive.py
+++ userbot/modules/google_drive.py
@@ -357,7 +357,7 @@ async def upload(gdrive, service, file_path, file_name, mimeType):
         pass
     body = {
         "name": file_name,
-        "description": "Uploaded from Telegram using ProjectBish userbot.",
+        "description": "Uploaded by frizzy from Telegram using ProjectBish userbot.",
         "mimeType": mimeType,
     }
     try:
