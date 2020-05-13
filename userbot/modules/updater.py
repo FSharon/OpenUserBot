c6a1a0aa98e3e98b2b27d9535104d1fac71815c8
frizzy <dhanikata@gmail.com>
2020-05-13 11:49:11 +0700

    updater: cleanup

--- userbot/modules/updater.py
+++ userbot/modules/updater.py
@@ -26,7 +26,10 @@ async def gen_chlog(repo, diff):
     ch_log = ''
     d_form = "%d/%m/%y"
     for c in repo.iter_commits(diff):
-        ch_log += f'•[{c.committed_datetime.strftime(d_form)}]: {c.summary} <{c.author}>\n'
+        ch_log += (
+            f'•[{c.committed_datetime.strftime(d_form)}]: '
+            f'{c.summary} <{c.author}>\n'
+        )
     return ch_log
 
 
@@ -62,7 +65,8 @@ async def deploy(event, repo, ups_rem, ac_br, txt):
                 break
         if heroku_app is None:
             await event.edit(
-                f'{txt}\n`Invalid Heroku credentials for deploying userbot dyno.`'
+                f'{txt}\n'
+                '`Invalid Heroku credentials for deploying userbot dyno.`
             )
             return repo.__del__()
         await event.edit('`[HEROKU]:'
@@ -79,7 +83,7 @@ async def deploy(event, repo, ups_rem, ac_br, txt):
             remote = repo.create_remote("heroku", heroku_git_url)
         try:
             remote.push(refspec="HEAD:refs/heads/master", force=True)
-        except GitCommandError as error:
+        except Exception as error:
             await event.edit(f'{txt}\n`Here is the error log:\n{error}`')
             return repo.__del__()
         await event.edit('`Successfully Updated!\n'
@@ -137,8 +141,10 @@ async def upstream(event):
     except InvalidGitRepositoryError as error:
         if conf is None:
             return await event.edit(
-                f"`Unfortunately, the directory {error} does not seem to be a git repository."
-                "\nBut we can fix that by force updating the userbot using .update now.`"
+                f"`Unfortunately, the directory {error} "
+                "does not seem to be a git repository.\n"
+                "But we can fix that by force updating the userbot using "
+                ".update now.`"
             )
         repo = Repo.init()
         origin = repo.create_remote('upstream', off_repo)
@@ -187,8 +193,8 @@ async def upstream(event):
             remove("output.txt")
         else:
             await event.edit(changelog_str)
-        return await event.respond('`do ".update now/deploy" to update`')
-
+        return await event.respond(
+            '`do ".update now or .update deploy" to update.`')
     if force_update:
         await event.edit(
             '`Force-Syncing to latest stable userbot code, please wait...`')
@@ -204,9 +210,11 @@ async def upstream(event):
 CMD_HELP.update({
     'update':
     ".update"
-    "\nUsage: Checks if the main userbot repository has any updates and shows a changelog if so."
+    "\nUsage: Checks if the main userbot repository has any updates "
+    "and shows a changelog if so."
     "\n\n.update now"
-    "\nUsage: Update your userbot, if there are any updates in your userbot repository."
+    "\nUsage: Update your userbot, "
+    "if there are any updates in your userbot repository."
     "\n\n.update deploy"
     "\nUsage: Deploy your userbot at heroku, if there are any updates in your userbot repository."
 })
