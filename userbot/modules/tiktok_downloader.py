cb0323a673fbbe6bd9dc353a740a178798958c16
frizzy <dhanikata@gmail.com>
2020-06-03 21:53:26 +0700

    try

--- userbot/modules/tiktok_downloader.py
+++ userbot/modules/tiktok_downloader.py
@@ -15,22 +15,28 @@ from userbot.events import register
 async def _(event):
     if event.fwd_from:
         return
-    link = event.pattern_match.group(1) 
+    d_link = event.pattern_match.group(1)
+    if ".com" not in d_link:
+        await event.edit("` I need a link to download something pro.`**(._.)**")
+    else:
+        await event.edit("**Initiating Download!**")
     chat = "@HK_tiktok_BOT"
-    await event.edit("```Processing```")
     async with bot.conversation(chat) as conv:
-          try:     
-              response = conv.wait_event(events.NewMessage(incoming=True,from_users=801042975))
-              await bot.send_message(chat, link)
-              response = await response 
-          except YouBlockedUserError: 
-              await event.reply("```Please unblock @HK_tiktok_BOT and try again```")
+          try:
+              msg_start = await conv.send_message("/start")
+              r = await conv.get_response()
+              msg = await conv.send_message(d_link)
+              details = await conv.get_response()
+              video = await conv.get_response()
+              """ - don't spam notif - """
+              await bot.send_read_acknowledge(conv.chat_id)
+          except YouBlockedUserError:
+              await event.edit("**Error:** `unblock` @HK_tiktok_BOT `and retry!`")
               return
-          if response.text.startswith("**Sorry I couldn't get TikTok video from**"):
-             await event.edit("```I think this is not the right link```")
-          else: 
-             await event.delete()   
-             await bot.send_message(event.chat_id, response.message)
+          await bot.send_file(event.chat_id, video)
+          await event.client.delete_messages(conv.chat_id,
+                                             [msg_start.id, r.id, msg.id, details.id, video.id])
+          await event.delete()
 
 CMD_HELP.update({
     "tiktok":
