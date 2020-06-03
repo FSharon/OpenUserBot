97e8ddcea47d5575dc8d906242f3001648067bb9
frizzy <dhanikata@gmail.com>
2020-06-03 18:28:11 +0700

    tiktok_downloader: try again

--- userbot/modules/tiktok_downloader.py
+++ userbot/modules/tiktok_downloader.py
@@ -17,30 +17,32 @@ from userbot.events import register
 async def _(event):
     if event.fwd_from:
         return
-    song = event.pattern_match.group(1)
+    d_link = event.pattern_match.group(1)
+    if ".com" not in d_link:
+        await event.edit("` I need a link to download something pro.`**(._.)**")
+    else:
+        await event.edit("**Initiating Download!**")
     chat = "@HK_tiktok_BOT"
-    await event.edit("```Getting your TikTok video```")
     async with bot.conversation(chat) as conv:
-          await asyncio.sleep(2)
-          await event.edit("`Downloading...Please wait`")
           try:
-              msg = await conv.send_message
+              msg_start = await conv.send_message("/start")
               response = await conv.get_response()
-              respond = await conv.get_response()
+              r = await conv.get_response()
+              msg = await conv.send_message(d_link)
+              details = await conv.get_response()
+              song = await conv.get_response()
               """ - don't spam notif - """
               await bot.send_read_acknowledge(conv.chat_id)
           except YouBlockedUserError:
-              await event.reply("```Please unblock @HK_tiktok_BOT and try again```")
+              await event.edit("**Error:** `unblock` @HK_tiktok_BOT `and retry!`")
               return
-          await event.edit("`Sending your TikTok video...`")
-          await asyncio.sleep(3)
-          await bot.send_file(event.chat_id, respond)
-    await event.client.delete_messages(conv.chat_id,
-                                       [msg.id, response.id, respond.id])
-    await event.delete()
+          await bot.send_file(event.chat_id, song, caption=details.text)
+          await event.client.delete_messages(conv.chat_id,
+                                             [msg_start.id, response.id, r.id, msg.id, details.id, song.id])
+          await event.delete()
 
 CMD_HELP.update({
     "tiktok":
     ".tiktok <Link>"
-    "\nUsage: Download TikTok video without watermark with @HK_tiktok_BOT"
+    "\nUsage: Download TikTok video without watermark using @HK_tiktok_BOT"
 }) 
