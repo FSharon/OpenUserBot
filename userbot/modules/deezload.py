73a283dec965430d962fd35c1d437f5252c89580
frizzy <dhanikata@gmail.com>
2020-06-20 20:51:52 +0700

    deezload: add msg when uploading

--- userbot/modules/deezload.py
+++ userbot/modules/deezload.py
@@ -1,81 +1,60 @@
-#    UniBorg (telegram userbot)
-#    Copyright (C) 2020 The Authors
- 
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
- 
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
- 
-#    DeezLoader is an attempt to archive songs and
-#    to serve the poor who can't afford legal copy of the songs.
-#    If you are capable of buying and
-#    spending money on songs in legal ways, please do so.
- 
-#    The Author(s) of this module are not responsible
-#    for the usage of this program by other people.
- 
-#    The Author(s) of this module do not recommend
-#    doing it illegally or against Deezer's Terms of Service
- 
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
- 
+# Copyright (C) 2020 The Authors UniBorg (telegram userbot)
+#
+# Licensed under the Raphielscape Public License, Version 1.d (the "License");
+# you may not use this file except in compliance with the License.
+#
 # requires: deezloader hachoir Pillow
 # Ported from UniBorg by AnggaR96s
- 
+
 import deezloader
 import os
 import shutil
 import time
- 
+
 from userbot.events import register
 from userbot import CMD_HELP, DEEZER_ARL_TOKEN, TEMP_DOWNLOAD_DIRECTORY
 from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
 from telethon.tl.types import DocumentAttributeAudio
- 
-@register(outgoing=True, pattern="^\.deez (.+?|) (FLAC|MP3\_320|MP3\_256|MP3\_128)")
+
+@register(outgoing=True, pattern="^\.deezload (.+?|) (FLAC|MP3\_320|MP3\_256|MP3\_128)")
 async def _(event):
     """DeezLoader by @An0nimia
     Ported for UniBorg by @SpEcHlDe"""
     if event.fwd_from:
         return
- 
+
     strings = {
         "name": "DeezLoad",
-        "arl_token_cfg_doc": "`ARL Token for Deezer`",
-        "invalid_arl_token": "`Please set the required variables for this module`",
-        "wrong_cmd_syntax": "`Bruh, now i think how far should we go. please terminate my Session 🥺`",
-        "server_error": "`We're experiencing technical difficulties.`",
-        "processing": "`Downloading..`"
+        "arl_token_cfg_doc": "ARL Token for Deezer",
+        "invalid_arl_token": "please set the required variables for this module",
+        "wrong_cmd_syntax": "bruh, now i think how far should we go. please terminate my Session 🥺",
+        "server_error": "We're experiencing technical difficulties.",
+        "processing": "`Downloading...`",
+        "uploading": "`Uploading...`"
     }
- 
+
     ARL_TOKEN = DEEZER_ARL_TOKEN
- 
+
     if ARL_TOKEN is None:
         await event.edit(strings["invalid_arl_token"])
         return
- 
+
     try:
         loader = deezloader.Login(ARL_TOKEN)
     except Exception as er:
         await event.edit(str(er))
         return
- 
+
     temp_dl_path = os.path.join(TEMP_DOWNLOAD_DIRECTORY, str(time.time()))
     if not os.path.exists(temp_dl_path):
         os.makedirs(temp_dl_path)
- 
+
     required_link = event.pattern_match.group(1)
     required_qty = event.pattern_match.group(2)
- 
+
     await event.edit(strings["processing"])
- 
+
     if "spotify" in required_link:
         if "track" in required_link:
             required_track = loader.download_trackspo(
@@ -86,10 +65,11 @@ async def _(event):
                 recursive_download=True,
                 not_interface=True
             )
+            await event.edit(strings["uploading"])
             await upload_track(required_track, event)
             shutil.rmtree(temp_dl_path)
             await event.delete()
- 
+
         elif "album" in required_link:
             reqd_albums = loader.download_albumspo(
                 required_link,
@@ -101,10 +81,11 @@ async def _(event):
                 zips=False
             )
             for required_track in reqd_albums:
+                await event.edit(strings["uploading"])
                 await upload_track(required_track, event)
             shutil.rmtree(temp_dl_path)
             await event.delete()
- 
+
     elif "deezer" in required_link:
         if "track" in required_link:
             required_track = loader.download_trackdee(
@@ -115,10 +96,11 @@ async def _(event):
                 recursive_download=True,
                 not_interface=True
             )
+            await event.edit(strings["uploading"])
             await upload_track(required_track, event)
             shutil.rmtree(temp_dl_path)
             await event.delete()
- 
+
         elif "album" in required_link:
             reqd_albums = loader.download_albumdee(
                 required_link,
@@ -130,14 +112,15 @@ async def _(event):
                 zips=False
             )
             for required_track in reqd_albums:
+                await event.edit(strings["uploading"])
                 await upload_track(required_track, event)
             shutil.rmtree(temp_dl_path)
             await event.delete()
- 
+
     else:
         await event.edit(strings["wrong_cmd_syntax"])
- 
- 
+
+
 async def upload_track(track_location, message):
     metadata = extractMetadata(createParser(track_location))
     duration = 0
@@ -171,10 +154,10 @@ async def upload_track(track_location, message):
         attributes=document_attributes,
     )
     os.remove(track_location)
- 
+
 CMD_HELP.update({
     "deezload":
-        ".deez <spotify/deezer link> format"
-        "\nUsage: Download music from deezer or spotify."
+        ".deezload <spotify/deezer link> <Format>"
+        "\nUsage: Download music from deezer."
+        "\n\n *Format= `FLAC`, `MP3_320`, `MP3_256`, `MP3_128`."
 })
- 
