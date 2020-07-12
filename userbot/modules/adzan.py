f0baa597374e977e2122da2b216fc2d5eabd25cd
frizzy <dhanikata@gmail.com>
2020-07-12 17:07:44 +0700

    Update adzan.py

--- userbot/modules/adzan.py
+++ userbot/modules/adzan.py
@@ -1,14 +1,16 @@
 import json
+
 import requests
 
 from userbot import CMD_HELP
 from userbot.events import register
 
-PLACE = ''
+PLACE = ""
 
 
-@register(pattern="^.adzan(?: |$)(.*)")
+@register(outgoing=True, pattern=r"^\.adzan (.*)")
 async def get_adzan(adzan):
+    await adzan.edit("🚛Loading...")
     if not adzan.pattern_match.group(1):
         LOCATION = PLACE
         if not LOCATION:
@@ -18,13 +20,13 @@ async def get_adzan(adzan):
         LOCATION = adzan.pattern_match.group(1)
 
     # url = f'http://muslimsalat.com/{LOKASI}.json?key=bd099c5825cbedb9aa934e255a81a5fc'
-    url = f'https://api.pray.zone/v2/times/today.json?city={LOCATION}'
+    url = f"https://api.pray.zone/v2/times/today.json?city={LOCATION}"
     request = requests.get(url)
     if request.status_code == 500:
         return await adzan.edit(f"Couldn't find city `{LOCATION}`")
-    
+
     parsed = json.loads(request.text)
-   
+
     city = parsed["results"]["location"]["city"]
     country = parsed["results"]["location"]["country"]
     timezone = parsed["results"]["location"]["timezone"]
@@ -37,19 +39,24 @@ async def get_adzan(adzan):
     maghrib = parsed["results"]["datetime"][0]["times"]["Maghrib"]
     isya = parsed["results"]["datetime"][0]["times"]["Isha"]
 
-    result = (f"**Jadwal Sholat**:\n"
-                 f"📅 `{date}`\n"
-                 f"📍 `{city} | {country}`\n\n"
-                 f"**Imsak :** `{imsak}`\n"
-                 f"**Subuh :** `{subuh}`\n"
-                 f"**Zuhur :** `{zuhur}`\n"
-                 f"**Ashar :** `{ashar}`\n"
-                 f"**Maghrib :** `{maghrib}`\n"
-                 f"**Isya :** `{isya}`\n")
+    result = (
+        f"**Jadwal Sholat**:\n"
+        f"📅 `{date} | {timezone}`\n"
+        f"🌏 `{city} | {country}`\n\n"
+        f"**Imsak :** `{imsak}`\n"
+        f"**Subuh :** `{subuh}`\n"
+        f"**Zuhur :** `{zuhur}`\n"
+        f"**Ashar :** `{ashar}`\n"
+        f"**Maghrib :** `{maghrib}`\n"
+        f"**Isya :** `{isya}`\n"
+    )
 
     await adzan.edit(result)
 
-CMD_HELP.update({
-        "adzan": ".adzan <city>\
+
+CMD_HELP.update(
+    {
+        "adzan": ".adzan **city**\
         \nUsage: Gets the prayer time for moslem."
-    })
+    }
+)
